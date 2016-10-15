355dab3e61e70b142be1662fc07a6fdc642b6e42
skycutecat <les.miserables309@gmail.com>
2016-10-15 15:00:36 +0800

    find.js 補充

--- find.js
+++ find.js
@@ -7,7 +7,6 @@ var age = Number(process.argv[2]);
 
 //connection info, database Name: learnyoumongo
 var url = "mongodb://localhost:27017/learnyoumongo";
-// console.log("age:" + age);
 mongo.connect(url, function(err, db){
 	//AssertionError: null == error. 若錯誤訊息不是null則跳出訊息
 	assert.equal(null, err);
@@ -15,18 +14,17 @@ mongo.connect(url, function(err, db){
 
 	//若insert 之 collection不存在,則會自動建立
 	//https://docs.mongodb.com/manual/reference/method/db.collection.insert/#create-collection
+	//ps. 本來是以為要自己建立一個	collection, 但是實際上已經題目已經有內建一個 collection 'parrots'
+
 	var parrots = db.collection('parrots');
-	//console.log(db.collection('parrots').find());
+
 	parrots.find({
+		// 查詢條件
 		age: { $gt: age }
 	}).toArray(function (err, items) {
 		assert.equal(null, err);
 		console.log(items)
 	});
-	// 以下為取得 db collections.
-	// db.listCollections().toArray(function (err,items){
-	// 	console.log(items.length)
-	// });
-	//close db
+
 	db.close();
 });
